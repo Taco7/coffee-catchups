74b915d3dcdc525e1cb65a3d1cc33f856cf86c73
Aditya Verma <adi.kiran98@gmail.com>
2019-09-17 17:32:15 +0530

    added api skeleton

--- main/main.go
+++ main/main.go
@@ -12,9 +12,26 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/profile", getProfile).Methods("GET")
+	router.HandleFunc("/pastmeetings", getPastMeetings).Methods("GET")
+	router.HandleFunc("/upcoming", getUpcoming).Methods("GET")
+	router.HandleFunc("/schedule", schedule).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func getProfile(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+func getPastMeetings(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+func getUpcoming(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+func schedule(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
