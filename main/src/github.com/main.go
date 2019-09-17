d1a64f91391d96cd68bc46db472b82f8aabc10d1
aish16 <aishwaryaraj16@gmail.com>
2019-09-18 02:39:24 +0530

    adding code for API- profile, upcoming and past meeting

--- main/src/github.com/main.go
+++ main/src/github.com/main.go
@@ -25,9 +25,9 @@ func main() {
 func configureRoutes(){
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/profile", getProfile).Methods("GET")
-	router.HandleFunc("/pastmeetings", getPastMeetings).Methods("GET")
-	router.HandleFunc("/upcoming", getUpcoming).Methods("GET")
+	router.HandleFunc("/profile/{employeeId}", getProfile).Methods("GET")
+	router.HandleFunc("/pastmeetings/{employeeId}", getPastMeetings).Methods("GET")
+	router.HandleFunc("/upcoming/{employeeId}", getUpcoming).Methods("GET")
 	router.HandleFunc("/schedule", schedule).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -59,13 +59,67 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func getProfile(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	
+	vars := mux.Vars(r)
+	userName := vars["employeeId"]
+	
+	for _, v := range blkEmployee.Employee {
+		if v.EmployeeId == userName {
+			b, err := json.Marshal(v)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			w.Write(b)
+			return
+		}
+	}
+	for _, v := range efrontEmployee.Employee {
+		if v.EmployeeId == userName {
+			b, err := json.Marshal(v)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			w.Write(b)
+			return
+		}
+	}
+	w.Write([]byte("User not found"))
 }
 func getPastMeetings(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	
+	vars := mux.Vars(r)
+	userName := vars["employeeId"]
+	for _, v := range meeting.MeetingInfo {
+		if v.EmployeeId == userName {
+			b, err := json.Marshal(v.SpokenTo)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			w.Write(b)
+			return
+		}
+	}
+	w.Write([]byte("User not found"))
 }
 func getUpcoming(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	
+	vars := mux.Vars(r)
+	userName := vars["employeeId"]
+	for _, v := range meeting.MeetingInfo {
+		if v.EmployeeId == userName {
+			b, err := json.Marshal(v.Upcoming)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			w.Write(b)
+			return
+		}
+	}
+	w.Write([]byte("User not found"))
 }
 func schedule(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
--- main/src/github.com/models/meeting.go
+++ main/src/github.com/models/meeting.go
@@ -2,15 +2,15 @@ package models
 
 type MeetingDetails struct {
 	EmployeeId string `json:"employeeId"`
-	SpokenTo []string `json:"spokenTo"`
-	Upcoming []string `json:"spokenTo"`
+	SpokenTo []Details `json:"spokenTo"`
+	Upcoming []Details `json:"upcomingMeeting"`
 }
 
 type Meeting struct{
 	MeetingInfo []MeetingDetails `json:"meetingInfo"`
 }
 
-type UpcomingDetails struct{
+type Details struct{
 	EmployeeId string `json:"employeeId"`
 	MeetingDate string `json:"date"`
 	MeetingTime string `json:"time"`
